46bee883d3ac82a6c4110a83d98775fa47c4f32d
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-12-10 22:20:30 +0800

    Update project1.bat

--- project1.bat
+++ project1.bat
@@ -1,1 +1,15 @@
+@echo off
+:: PROJECT 1 - Sistem Manajemen File Pribadi
 
+echo Membuat struktur folder MyFiles...
+mkdir D:\MyFiles
+mkdir D:\MyFiles\Documents
+mkdir D:\MyFiles\Pictures
+mkdir D:\MyFiles\Videos
+mkdir D:\MyFiles\Music
+mkdir D:\MyFiles\Projects
+
+echo Struktur selesai dibuat!
+echo Lihat struktur folder:
+tree D:\MyFiles
+pause
